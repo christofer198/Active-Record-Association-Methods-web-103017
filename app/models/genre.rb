df017ebfa3b0d88012b819ffa418dafc1a0a2c99
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 17:25:50 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,7 +16,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
+    #binding.pry
     instance = Artist.select(:name)
     instance.collect{|x| x.name}
   end
