56cb87b46f7ad09792cc8cf0f0d32ed27629fb94
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-13 16:43:57 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,6 +6,7 @@ class Artist < ActiveRecord::Base
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
     Artist.where(id: self.id).limit(1)
+    binding.pry
   end
 
   def song_count
